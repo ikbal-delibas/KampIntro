24b6a55dfeb54e39222bd630be75c9fd8954915b
İkbal Delibaş <ikbal53@hotmail.com>
2023-10-19 12:41:07 +0300

    Add project files.

---
+++ KampIntro/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductComment { get; set; }
+    }
+}
