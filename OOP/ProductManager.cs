9f7324e6019c9d8d491f68f0e6bf77362f137128
İkbal Delibaş <ikbal53@hotmail.com>
2023-10-24 16:28:32 +0300

    a

---
+++ OOP/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+        public void UdateAdd(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,7 +6,24 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitePrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product()
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Masa",
+                UnitePrice = 500,
+                UnitsInStock = 3
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
         }
     }
 }
