151e74d515eaec739d760e1bb3ce9abe247713c1
İkbal Delibaş <ikbal53@hotmail.com>
2023-10-24 17:04:45 +0300

    a

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("İkbal");
+            myList.Add("Osman");
+            myList.Add("İkbal");
+            myList.Add("İkbal");
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>();
+            isimler.Add("İkbal");
+            isimler.Add("Osman");
+            isimler.Add("Aynur");
+            isimler.Add("Engin");
+            isimler.Add("Havva");
+            isimler.Add("Nur");
+            foreach (var isim in isimler)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("************");
+            for (int i = 0; i < isimler.Count; i++)
+            {
+                Console.WriteLine(isimler[i]);
+            }
+
+        }
+    }
+}
